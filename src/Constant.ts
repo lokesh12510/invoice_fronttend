da786342282d793a2ca4604b818054e8dfd72a23
pofilokesh <82874155+pofilokesh@users.noreply.github.com>
2022-03-07 13:31:27 +0530

    url

--- src/Constant.ts
+++ src/Constant.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3001/api/";
-// export const BASE_URL = "https://mysql-invoice-db.herokuapp.com/api/";
+// export const BASE_URL = "http://localhost:3001/api/";
+export const BASE_URL = "https://mysql-invoice-db.herokuapp.com/api/";
